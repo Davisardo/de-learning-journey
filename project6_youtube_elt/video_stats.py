8e438ce51cd360418713a2359f553a5c4e2f8673
DavisArdo <davisardo6@gmail.com>
2026-08-21 08:28:37 +0700

    Add get_playlist_id function with dotenv for API key security

---
+++ project6_youtube_elt/video_stats.py
@@ -0,0 +1,41 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+load_dotenv("./.env")
+
+
+
+api_key = os.getenv("YOUTUBE_API_KEY")
+channel_handle = "MrBeast"
+
+
+def get_playlist_id():
+
+    try:
+        url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={channel_handle}&key={api_key}"
+
+        response = requests.get(url)
+
+        response.raise_for_status()
+
+        data = response.json()
+
+        # print(json.dumps(data,indent=4))
+
+        channel_items = data["items"][0]
+
+        channel_playlistid = channel_items["contentDetails"]["relatedPlaylists"][
+            "uploads"
+        ]
+
+        # print(channel_playlistid)
+
+        return channel_playlistid
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+
+if __name__ == "__main__":
+    print(get_playlist_id())
