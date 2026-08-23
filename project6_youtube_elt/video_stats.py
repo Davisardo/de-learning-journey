fd7e2f0f26d6395909d7e6216c46b4699d113aa0
DavisArdo <davisardo6@gmail.com>
2026-08-23 10:40:32 +0700

    Add save_to_json function to complete extract pipeline

--- project6_youtube_elt/video_stats.py
+++ project6_youtube_elt/video_stats.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from datetime import date
 import os
 from dotenv import load_dotenv
 
@@ -104,8 +105,15 @@ def extract_video_data(video_ids):
         raise e
 
 
+def save_to_json(extracted_data):
+    file_path = f"./data/youtube_data_{date.today()}.json"
+
+    with open(file_path, "w", encoding="utf-8") as json_file:
+        json.dump(extracted_data, json_file, indent=4, ensure_ascii=False)
+
+
 if __name__ == "__main__":
     playlist_id = get_playlist_id()
     video_ids = get_video_id(playlist_id)
     extracted_data = extract_video_data(video_ids)
-    # print(extracted_data)
+    save_to_json(extracted_data)
